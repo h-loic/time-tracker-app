292163074a3e245e868e212dca1aa49f015379d0
h-loic <h-loic@orange.fr>
2023-09-18 15:17:13 +0200

    added tasks to logHours

--- app/logHours/page.js
+++ app/logHours/page.js
@@ -22,18 +22,12 @@ export default function logHours(){
 
     const router = useRouter();
 
-    const taskHours = {task : "tache1", hours : 0}
-    const chantierTaskHours = [taskHours];
+    const taskHours = {task : "", hours : 0}
+    const chantierTaskHours = [{task : "atelier", hours : 0},{task : "chantier", hours : 0}, {task : "regie", hours : 0}];
 
-    const [startDate, setStartDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
     const [worker, setWorker] = useState({});
     const [chantiers, setChantiers] = useState([]);
-    const [isDateValidate, setDateValidate] = useState(false);
-
-    const [formData, setFormData] = useState({
-        numberOfHours: 0,
-        chantierId: null,
-    });
 
     const [loaded,setLoaded] = useState(false)
 
@@ -50,10 +44,6 @@ export default function logHours(){
                 itemsArr.push({ ...doc.data(), id: doc.id });
               });
               setChantiers(itemsArr);
-              setFormData({
-                numberOfHours : 0,
-                chantierId : itemsArr[0].id
-              })
               setloggedChantiers(oldArray => [...oldArray, {chantier : itemsArr[0].id, taskHours : chantierTaskHours}]);
               return () => unsubscribe();
             });
@@ -74,51 +64,68 @@ export default function logHours(){
       }, [session]);
 
     const handleFormSubmit = async () => {
-      console.log(loggedChantiers)
-      /*
-        const docRef1 = doc(db, `workers/${worker.id}/chantiers/${formData.chantierId}`)
-        const document = await getDoc(docRef1);
-        if (document.exists()){
-            await updateDoc(docRef1, {"chantierId" : formData.chantierId,"chantierHours" : increment(formData.numberOfHours)})
-        }else{
-            await setDoc(docRef1, {"chantierId" : formData.chantierId,"chantierHours" : increment(formData.numberOfHours)})
-        }
-        const docRef2 = doc(db, `workers`,worker.id)
-        updateDoc(docRef2, {
-            totalHours : increment(formData.numberOfHours)
-        })
+        let totalHours = 0;
 
-        const docRef3 = doc(db,"chantiers", formData.chantierId)
-        updateDoc(docRef3,{
-            availableHours : increment(-formData.numberOfHours),
-            usedHours : increment(formData.numberOfHours)
-        })
+        loggedChantiers.forEach(async chantier => {
+          chantier.taskHours.forEach(async task => {
+            totalHours+= parseFloat(task.hours)
+            const docRef5 = doc(db, `chantiers/${chantier.chantier}/tasks/${task.task}`)
+            const document5 = await getDoc(docRef5);
+            if (document5.exists()){
+                await updateDoc(docRef5, {"hours" : increment(task.hours)})
+            }else{
+                await setDoc(docRef5, {"hours": task.hours})
+            }
+          });
+        });
 
-        const docRef4 = doc(db, `chantiers/${formData.chantierId}/workers/${worker.id}`)
-        const document4 = await getDoc(docRef4);
-        if (document4.exists()){
-            await updateDoc(docRef4, {"name": worker.name,"workerId" : worker.id,"workedHours" : increment(formData.numberOfHours)})
-        }else{
-            await setDoc(docRef4, {"name": worker.name,"workerId" : worker.id,"workedHours" : increment(formData.numberOfHours)})
-        }
+        loggedChantiers.forEach(async chantier => {
+          const docRef1 = doc(db, `workers/${worker.id}/chantiers/${chantier.chantier}`)
+          const document = await getDoc(docRef1);
+          if (document.exists()){
+              await updateDoc(docRef1, {"chantierId" : chantier.chantier,"chantierHours" : increment(totalHours)})
+          }else{
+              await setDoc(docRef1, {"chantierId" : chantier.chantier,"chantierHours" : totalHours})
+          }
+          const docRef2 = doc(db, `workers`,worker.id)
+          updateDoc(docRef2, {
+              totalHours : increment(totalHours)
+          })
+  
+          const docRef3 = doc(db,"chantiers", chantier.chantier)
+          updateDoc(docRef3,{
+              availableHours : increment(-totalHours),
+              usedHours : increment(totalHours)
+          })
+  
+          const docRef4 = doc(db, `chantiers/${chantier.chantier}/workers/${worker.id}`)
+          const document4 = await getDoc(docRef4);
+          if (document4.exists()){
+              await updateDoc(docRef4, {"name": worker.name,"workerId" : worker.id,"workedHours" : totalHours})
+          }else{
+              await setDoc(docRef4, {"name": worker.name,"workerId" : worker.id,"workedHours" : increment(totalHours)})
+          }
+
+          const docRef6 = doc(db, `workers/${worker.id}/workedDay/${date}`)
+          const document6 = await getDoc(docRef6);
+          if (document6.exists()){
+              await updateDoc(docRef6, {"hours" : totalHours })
+          }else{
+              await setDoc(docRef6, {"hours" : increment(totalHours) })
+          }
+        })
 
         router.back();
-        */
     };
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-    };
-
-    const handleChantierChange = (oldChantierIndex, newChantierId) => {
+    const handleChantierChange = (oldChantierIndex, e) => {
+      const { value } = e.target;
       if (loggedChantiers[oldChantierIndex].taskHours == undefined){
-        loggedChantiers[oldChantierIndex] = {chantier : newChantierId, taskHours : chantierTaskHours};
+        loggedChantiers[oldChantierIndex] = {chantier : value, taskHours : chantierTaskHours};
       }else{
-        loggedChantiers[oldChantierIndex].chantier = newChantierId;
+        loggedChantiers[oldChantierIndex].chantier = value;
       }
       setloggedChantiers(loggedChantiers)
-      console.log(loggedChantiers)
     } 
 
     const handleTaskChange = (event, chantierIndex, taskIndex) => {
@@ -137,10 +144,22 @@ export default function logHours(){
       setloggedChantiers(oldArray => [...oldArray, {chantier : chantiers[0].id, taskHours : chantierTaskHours}]);
     }
 
+    const removechantier = (index) => {
+      loggedChantiers.splice(index,1);
+      setloggedChantiers(oldArray => [...loggedChantiers]);
+      console.log(loggedChantiers)
+    }
+
     const addOtherTask = (index) => {
       loggedChantiers[index].taskHours.push(taskHours)
-      setloggedChantiers(loggedChantiers);
-      setloggedChantiers(oldArray => [...oldArray]);
+      //setloggedChantiers(loggedChantiers);
+      setloggedChantiers(oldArray => [...loggedChantiers]);
+
+    }
+
+    const removeTask = (chantierIndex,taskIndex) => {
+      loggedChantiers[chantierIndex].taskHours.splice(taskIndex,1);
+      setloggedChantiers(oldArray => [...loggedChantiers]);
     }
 
     return(
@@ -153,45 +172,77 @@ export default function logHours(){
                 <DatePicker
                     className="items-center border-solid border-2 border-blue-600 text-center font-bold"
                     showIcon
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
+                    selected={date}
+                    onChange={(date) => setDate(date)}
                   />
               </div>
               <br/><br/>
                 <div>
                     { loggedChantiers.map((loggedChantier, index) => (
-                      <div key={loggedChantier.chantier}>
+                      <div key={index}>
                         <div className='border-solid border-2 border-slate-800 p-3'>
                         <label htmlFor="chantierId" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionné le chantier</label>
-                          <select id="chantierId" value={formData.isFinished} name="chantierId" onChange={() => handleChantierChange(index, loggedChantier.chantier)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                          <select id="chantierId" value={chantiers.isFinished} name="chantierId" onChange={(e) => handleChantierChange(index, e)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                               { chantiers.map((chantier) => (
                                   <option key={chantier.id} value={chantier.id}>{chantier.name}</option>
                               ))}
                           </select>
                           <br/>
-                          <div>entré vos tâches</div>
-                          { loggedChantier.taskHours.map((task,taskIndex) =>(
-                            <div key={task.task} className='grid grid-cols-2 gap-4 mb-2'>
+                          <div>entré vos heures :</div>
+                          <div className='grid grid-cols-2 gap-4 mb-2'>
+                              <div className=''>Atelier </div>
+                              <div className="">
+                                <input placeholder="entré le nombre d'heures"
+                                onChange={(e) => handleNumberHoursChange(e, index, 0)} 
+                                type="number" name="numberOfHours" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                              </div>
+                          </div>
+                          <div className='grid grid-cols-2 gap-4 mb-2'>
+                              <div className=''>Chantier </div>
+                              <div className="">
+                                <input placeholder="entré le nombre d'heures" 
+                                onChange={(e) => handleNumberHoursChange(e, index, 1)} 
+                                type="number" name="numberOfHours" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                              </div>
+                          </div>
+                          <div className='grid grid-cols-2 gap-4 mb-2'>
+                              <div className=''>Regie </div>
+                              <div className="">
+                                <input placeholder="entré le nombre d'heures" 
+                                onChange={(e) => handleNumberHoursChange(e, index, 2)} 
+                                type="number" name="numberOfHours" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                              </div>
+                          </div>
+                          {loggedChantier.taskHours.length > 3 ? loggedChantier.taskHours.map((task,taskIndex) =>(
+                            <>
+                            { taskIndex < 3 ? <></> :
+                            <div key={task.taskIndex} className='grid grid-cols-3 gap-4 mb-2'>
                               <div>
-                              <select id="tache" name="chantierId" onChange={(e) => handleTaskChange(e,index,taskIndex)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option value="tache1">tache 1</option>
-                                <option value="tache2">tache 2</option>
-                                <option value="tache3">tache 3</option>
-                                <option value="tache4">tache 4</option>
-                                <option value="autre">autre</option>
-                              </select>
+                                <input id={taskIndex + index} name="chantierId" onChange={(e) => handleTaskChange(e,index,taskIndex)}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                                </input>
                               </div>
                               <div className="">
                                 <input placeholder="entré le nombre d'heures" 
                                 onChange={(e) => handleNumberHoursChange(e, index, taskIndex)} 
                                 type="number" name="numberOfHours" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                               </div>
-                          </div>
-                          ))
+                              <button onClick={() => removeTask(index,taskIndex)}
+                              type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                Supprimer
+                                </button>
+                            </div>
+                           }
+                           </>
+                          )) : <></>
                           } 
                           <button type='button' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                           onClick={() => addOtherTask(index)}
                           >ajouter une tache</button>
+                          <button onClick={() => removechantier(index)}
+                            type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                            Supprimer les données de ce chantier
+                          </button>
                         </div>
                         <br/>
                       </div>
